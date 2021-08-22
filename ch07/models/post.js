6635a327821172ae082b954b32bfd69f4e721851
inhalin <inhalin.kim@gmail.com>
2021-08-23 00:12:37 +0900

    mysql sequelize server simple CRUD example

---
+++ ch07/models/post.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      hit: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        defaultValue: 0,
+      },
+    }, {
+      sequelize,
+      modelName: 'Post',
+      timestamps: true,
+    });
+  }
+  static associate(db) {
+    Post.belongsTo(db.User, {
+      foreignKey: 'user_id',
+      targetKey: 'id',
+    });
+  }
+}
---
+++ ch07/models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER.UNSIGNED,
+      },
+      info: {
+        type: Sequelize.TEXT,
+      },
+      is_admin: {
+        type: Sequelize.TINYINT,
+        defaultValue: 0,
+      },
+    }, {
+      sequelize,
+      modelName: 'User',
+      timestamps: true,
+    });
+  }
+  static associate(db) {
+    User.hasMany(db.Post, {
+      foreignKey: 'user_id',
+      sourceKey: 'id',
+    });
+  }
+}
